da32b79830dd6dc82d62c0549f3e5051e57e24a3
Andrew Trice <tricead@g.cofc.edu>
2016-11-24 17:14:20 -0500

    Create TwoSum.java
    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
    
    You may assume that each input would have exactly one solution.

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target && i != j) {
+                    ans[1] = i;
+                    ans[0] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
